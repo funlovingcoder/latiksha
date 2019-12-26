370685c4ad574f7d6454fc805c786a6b9b250767
Lalit Kishor <lkishor@deloitte.com>
2019-12-26 18:23:59 +0530

    bride details updated

--- index.php
+++ index.php
@@ -98,10 +98,7 @@
                         </div>
                         <div class="desc-bride">
                             <h3>Titiksha Sharma</h3>
-                            <p>Hey folks. The name's Lalit, but people in my family call me Piyush. I work as a
-                                Salesforce Consultant at Deloitte USI(Bangaluru) on weekdays, and try to take advantage
-                                of the outdoors and all the other fun(sleeping mostly) on weekends. I am an extrovert
-                                and love to have fun and good food to eat and that is all to make a day good for me.</p>
+                            <p>Hi. The name is Titiksha, but my family and friends call me Neha. I have completed my engineering(EC) from MAIIT Kota, I see no difference between weekday and weekend I enjoy everyday like 'this is the day'. In my free time I enjoy going out with friends, I love dancing and sleeping. my favourite life mantra is to enjoy everyday.</p>
                             <p>← I kind of have a thing for Him</p>
                         </div>
                     </div>
